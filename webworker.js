85a1affaac92007f969c1f1c7c9dfc29296c4324
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-01-06 21:14:40 -0200

    Change some var declarations

--- webworker.js
+++ webworker.js
@@ -1,12 +1,12 @@
 function summation(n) {
-  var result = 0;
-  for (var i = 0; i <= n; i++) {
+  let result = 0;
+  for (let i = 0; i <= n; i++) {
     result += i;
   }
   return result;
 }
 
 self.addEventListener('message', function(e) {
-  var result = summation(e.data);
+  const result = summation(e.data);
   self.postMessage(result);
 }, false);
